c587e066f1d5abaf1e15d425a155a8371585a458
Jeff EKlund <jeff.eklund@gmail.com>
2012-07-17 13:16:53 +0200

    Update readme link in metadata

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer       "Jeff Eklund"
 maintainer_email "jeff.eklund@gmail.com"
 license          "Apache 2.0"
 description      "Check out git repo/branch and use rpmbuild to build rpm"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.org'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1"
 depends          "rpmbuild"
 supports         "centos"
